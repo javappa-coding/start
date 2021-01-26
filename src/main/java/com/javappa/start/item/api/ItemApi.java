06070b661deeb991ace1431a6015f41bc4f78c61
Jarosław Klimas <admin@javappa.com>
2021-01-26 11:36:19 +0100

    Initial commit

---
+++ src/main/java/com/javappa/start/item/api/ItemApi.java
@@ -0,0 +1,26 @@
+package com.javappa.start.item.api;
+
+import com.javappa.start.item.api.request.ItemRequest;
+import com.javappa.start.item.api.response.ItemResponse;
+import com.javappa.start.item.domain.Item;
+import com.javappa.start.item.service.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemApi {
+
+    private final ItemService itemService;
+
+    ItemApi(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemResponse> create(@RequestBody ItemRequest itemRequest) {
+        ItemResponse itemResponse = itemService.create(itemRequest);
+        return new ResponseEntity<>(itemResponse, HttpStatus.CREATED);
+    }
+}
