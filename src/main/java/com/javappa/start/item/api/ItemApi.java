74aa2e68380454a81b1e2ea2020b46691d044a0d
Jarosław Klimas <admin@javappa.com>
2021-01-31 17:24:44 +0100

    Dodanie Swaggera2

--- src/main/java/com/javappa/start/item/api/ItemApi.java
+++ src/main/java/com/javappa/start/item/api/ItemApi.java
@@ -3,11 +3,14 @@ package com.javappa.start.item.api;
 import com.javappa.start.item.api.request.ItemRequest;
 import com.javappa.start.item.api.response.ItemResponse;
 import com.javappa.start.item.service.ItemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(tags = "Items")
 @RequestMapping("/api/items")
 public class ItemApi {
 
@@ -18,6 +21,7 @@ public class ItemApi {
     }
 
     @PostMapping
+    @ApiOperation("Create item")
     public ResponseEntity<ItemResponse> create(@RequestBody ItemRequest itemRequest) {
         ItemResponse itemResponse = itemService.create(itemRequest);
         return new ResponseEntity<>(itemResponse, HttpStatus.CREATED);
